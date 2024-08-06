b96b5db99b082eded355ddcce0c0999bf731e3e0
zaldebro <153065276+zaldebro@users.noreply.github.com>
2024-08-06 22:41:51 +0800

    Update falcon_way.go

--- falcon/falcon_way.go
+++ falcon/falcon_way.go
@@ -11,20 +11,12 @@ import (
 type OnCallGroInfo struct {
 	ID         int       `json:"id"`
 	Name       string    `json:"name"`
-	Note       string    `json:"note"`
-	User       string    `json:"user"`
-	DutyUsers  []string  `json:"duty_users"`
-	Manager    string    `json:"manager"`
-	Cycle      int       `json:"cycle"`
-	ChatID     string    `json:"chat_id"`
-	TeamID     int       `json:"team_id"`
-	Creator    string    `json:"creator"`
-	CreateTime time.Time `json:"create_time"`
+xxxxxxxx
 }
 
 // GetOnCallGroInfo 获取 OnCall 组信息
 func (f *FALCON) GetOnCallGroInfo (onCallGroName string) (*OnCallGroInfo, error) {
-	path := fmt.Sprintf("/oncall/groups?oncallGroup=%s&mine=false&limit=2&offset=0", onCallGroName)
+	path := fmt.Sprintf("xxxxxxxxxxoncallGroup=%s&mine=false&limit=2&offset=0", onCallGroName)
 	if res, err := f.GetInfos(path); err == nil {
 		var onCallGroInfoList []OnCallGroInfo
 		err = json.Unmarshal(*res, &onCallGroInfoList)
@@ -50,32 +42,11 @@ func (f *FALCON) GetOnCallGroInfo (onCallGroName string) (*OnCallGroInfo, error)
 type Expression struct {
 	ID                 int         `json:"id"`
 	MetricName         string      `json:"metric_name"`
-	IncludeLabels      []string    `json:"include_labels"`
-	ExcludeLabels      []string `json:"exclude_labels"`
-	ExcludeFunc        string      `json:"exclude_func"`
-	Type        	   int      `json:"type"`
-	Period		       int       `json:"period"`
-	Callback		   int   `json:"callback"`
-	Op                 string      `json:"op"`
-	MaxStep            int         `json:"max_step"`
+xxxxxxx
 	Func               string      `json:"func"`
-	Pause              int         `json:"pause"`
-	Uic                string      `json:"uic"`
-	SendSms            int         `json:"send_sms"`
-	SendMail           int         `json:"send_mail"`
-	BeforeCallbackMail int         `json:"before_callback_mail"`
-	AfterCallbackMail  int         `json:"after_callback_mail"`
-	URL                string      `json:"url"`
-	Msg                string      `json:"msg"`
-	Creator            string      `json:"creator"`
-	RunBegin           string      `json:"run_begin"`
-	RunEnd             string      `json:"run_end"`
-	OrgID              int         `json:"org_id"`
+xxxxx
 	Thresholds         []struct {
-		ID        int    `json:"id"`
-		Priority  int    `json:"priority"`
-		Threshold string `json:"threshold"`
-		RunBegin  string `json:"run_begin"`
+		xxxxxxxxx
 		RunEnd    string `json:"run_end"`
 	} `json:"thresholds"`
 	OncallGroupName string      `json:"oncall_group_name"`
@@ -103,7 +74,7 @@ func (f *FALCON) GetExpressionsByOncallGro (onCallGroupName string) (*Expression
 	// 定义并发部分
 	emit := func(offset int) {
 		defer wg.Done()
-		path := fmt.Sprintf("/expression/oncall/search?oncall=%s&limit=%d&offset=%v", onCallGroupName, limit, offset)
+		path := fmt.Sprintf("xxxxxxxxxx?oncall=%s&limit=%d&offset=%v", onCallGroupName, limit, offset)
 		if ans, err := f.GetInfos(path); err == nil{
 			var expressionsByOncallGro ExpressionsByOncallGroStruct
 			err = json.Unmarshal(*ans, &expressionsByOncallGro)
@@ -120,7 +91,7 @@ func (f *FALCON) GetExpressionsByOncallGro (onCallGroupName string) (*Expression
 	}
 
 	// 默认 limit 为 100
-	path := fmt.Sprintf("/expression/oncall/search?oncall=%s&limit=%d", onCallGroupName, limit)
+	path := fmt.Sprintf("xxxxxxxxxxxxxxxh?oncall=%s&limit=%d", onCallGroupName, limit)
 
 	if body, err := f.GetInfos(path); err == nil {
 		err = json.Unmarshal(*body, &expressionsByOncallGroList)
@@ -148,7 +119,7 @@ func (f *FALCON) GetExpressionsByOncallGro (onCallGroupName string) (*Expression
 
 // GetExpressionByExprID 通过 ID 获取告警表达式
 func (f *FALCON) GetExpressionByExprID (exprID int) (*Expression, error) {
-	path := fmt.Sprintf("/expression/search?id=%d",exprID)
+	path := fmt.Sprintf("xxxxxx?id=%d",exprID)
 	if body , err := f.GetInfos(path); err == nil {
 		var expressionByExprID ExpressionsByOncallGroStruct
 		err = json.Unmarshal(*body, &expressionByExprID)
@@ -165,41 +136,14 @@ func (f *FALCON) GetExpressionByExprID (exprID int) (*Expression, error) {
 
 type PutUpdateInfo struct {
 	Action struct {
-		AfterCallbackMail  int    `json:"after_callback_mail"`
-		BeforeCallbackMail int    `json:"before_callback_mail"`
-		Callback           int    `json:"callback"`
-		Uic                string `json:"uic"`
-		Url                string `json:"url"`
-		OncallGroupId      int    `json:"oncall_group_id"`
-		OncallGroups       []OncallGroups `json:"oncall_groups"`
+	xxxxxxxxxx_groups"`
 	} `json:"action"`
 	Expression struct {
 		Metric        string   `json:"metric"`
-		IncludeLabels []string `json:"include_labels"`
-		ExcludeLabels []string `json:"exclude_labels"`
-		ExcludeFunc   string   `json:"exclude_func"`
-		Type          int      `json:"type"`
-		Period        int      `json:"period"`
-		Op            string   `json:"op"`
-		MaxStep       int      `json:"max_step"`
-		Msg           string   `json:"msg"`
-		Func          string   `json:"func"`
-		Pause         int      `json:"pause"`
-		//OrgId         int      `json:"org_id"`
-		Creator		  string   `json:"creator"`
-		RunBegin	  string    `json:"run_begin"`
-		RunEnd        string    `json:"run_end"`
-		OncallGroupName string      `json:"oncall_group_name"`
-		OncallGroupID   int         `json:"oncall_group_id"`
-		OncallGroups    interface{} `json:"oncall_groups"`
-		BeforeCallbackMail int         `json:"before_callback_mail"`
-		AfterCallbackMail  int         `json:"after_callback_mail"`
+xxxxxxxxlback_mail"`
 		OrgID              int         `json:"org_id"`
 		AddThresholds []struct {
-			Priority  int    `json:"priority"`
-			Threshold string `json:"threshold"`
-			RunBegin  string `json:"run_begin"`
-			RunEnd    string `json:"run_end"`
+			xxxxxx
 		} `json:"add_thresholds"`
 		UpdateThresholds []UpdateThreshold `json:"update_thresholds"`
 	} `json:"expression"`
@@ -207,10 +151,7 @@ type PutUpdateInfo struct {
 
 type UpdateThreshold struct {
 	Id        int    `json:"id"`
-	Priority  int    `json:"priority"`
-	Threshold string `json:"threshold"`
-	RunBegin  string `json:"run_begin"`
-	RunEnd    string `json:"run_end"`
+	xxxxxxxxx
 }
 
 type OncallGroups struct {
@@ -224,44 +165,7 @@ func (f *FALCON) ChangeExprLabels (alertExpressionInfo *Expression) bool {
 	var putUpdateInfo PutUpdateInfo
 
 	// 动作部分 Action，
-	//putUpdateInfo.Action.Uic = "testPoint" // 这个参数暂时不加上
-	putUpdateInfo.Action.Url = (*alertExpressionInfo).URL
-	putUpdateInfo.Action.Callback = (*alertExpressionInfo).Callback
-	putUpdateInfo.Action.BeforeCallbackMail = (*alertExpressionInfo).BeforeCallbackMail
-	putUpdateInfo.Action.AfterCallbackMail = (*alertExpressionInfo).AfterCallbackMail
-	putUpdateInfo.Action.OncallGroupId =  (*alertExpressionInfo).OncallGroupID
-
-	var oncallGroups OncallGroups
-	oncallGroups.ID = (*alertExpressionInfo).OncallGroupID
-	oncallGroups.Name = (*alertExpressionInfo).OncallGroupName
-	putUpdateInfo.Action.OncallGroups = append(putUpdateInfo.Action.OncallGroups, oncallGroups)
-
-	// 表达式部分，Expression
-	putUpdateInfo.Expression.Metric = (*alertExpressionInfo).MetricName
-	putUpdateInfo.Expression.Creator = (*alertExpressionInfo).Creator
-	putUpdateInfo.Expression.RunBegin = (*alertExpressionInfo).RunBegin
-	putUpdateInfo.Expression.RunEnd = (*alertExpressionInfo).RunEnd
-	putUpdateInfo.Expression.IncludeLabels = (*alertExpressionInfo).IncludeLabels
-	putUpdateInfo.Expression.ExcludeLabels = (*alertExpressionInfo).ExcludeLabels
-	putUpdateInfo.Expression.ExcludeFunc = (*alertExpressionInfo).ExcludeFunc
-	putUpdateInfo.Expression.Type = (*alertExpressionInfo).Type
-	putUpdateInfo.Expression.Period = (*alertExpressionInfo).Period
-	putUpdateInfo.Expression.Op = (*alertExpressionInfo).Op
-	putUpdateInfo.Expression.MaxStep = (*alertExpressionInfo).MaxStep
-	putUpdateInfo.Expression.Msg = (*alertExpressionInfo).Msg
-	putUpdateInfo.Expression.Func = (*alertExpressionInfo).Func
-	putUpdateInfo.Expression.Pause = (*alertExpressionInfo).Pause
-	putUpdateInfo.Expression.OrgID = (*alertExpressionInfo).OrgID
-	putUpdateInfo.Expression.AddThresholds = nil
-	for _, threshold := range (*alertExpressionInfo).Thresholds {
-		var updateThreshold UpdateThreshold
-		updateThreshold.Id = threshold.ID
-		updateThreshold.Priority = threshold.Priority
-		updateThreshold.Threshold = threshold.Threshold
-		updateThreshold.RunBegin = threshold.RunBegin
-		updateThreshold.RunEnd = threshold.RunEnd
-		putUpdateInfo.Expression.UpdateThresholds = append(putUpdateInfo.Expression.UpdateThresholds, updateThreshold)
-	}
+	xxxxxxxxxxxxxxx
 
 	// 修改之前存储一下日志
 	recordPath := fmt.Sprintf("/expression/search?id=%d",(*alertExpressionInfo).ID)
@@ -286,7 +190,7 @@ func (f *FALCON) ChangeExprLabels (alertExpressionInfo *Expression) bool {
 	}
 }
 
-// ModExprLabels  bool 表示是否是 mife
+// ModExprLabels  bool 表示是否是 xxxx
 // 当返回 map 为 nil 或者 bool 为 真时，均不需要修改
 func (f *FALCON) ModExprLabels (includeLabels, modList *[]string) (*[]string, bool) {
 	if *includeLabels == nil {
@@ -300,11 +204,11 @@ func (f *FALCON) ModExprLabels (includeLabels, modList *[]string) (*[]string, bo
 		key := tmpLabel[0]
 		vals := strings.Split(tmpLabel[1], ",")
 
-		// 如果有标签 owt=mfe，则不进行修改
-		if key == "owt" {
+		// 如果有标签 xxx=xxxx，则不进行修改
+		if key == "xxxx" {
 			for _, val := range vals {
-				if val == "mfe" {
-					//fmt.Println("label含有mfe")
+				if val == "xxxxxx" {
+					//fmt.Println("label含有xxxx")
 					return nil, true
 				}
 			}
@@ -350,14 +254,7 @@ func (f *FALCON) ModExprLabels (includeLabels, modList *[]string) (*[]string, bo
 }
 
 type TemplatesByOnCallGroStruct struct {
-	ID              int    `json:"id"`
-	Pid             int    `json:"pid"`
-	Name            string `json:"name"`
-	Pname           string `json:"pname"`
-	Creator         string `json:"creator"`
-	Uic             string `json:"uic"`
-	OncallGroupID   int    `json:"oncall_group_id"`
-	OncallGroupName string `json:"oncall_group_name"`
+	xxxxxxxxxxxx
 }
 
 // GetTemplatesByOnCallGro 根据 onCall组 获取告警模板
@@ -378,7 +275,7 @@ func (f *FALCON) GetTemplatesByOnCallGro (onCallGroupName string) (*[]TemplatesB
 			<- guard
 			wg.Done()
 		}()
-		path := fmt.Sprintf("/template/oncall/search?name=%s&limit=10&offset=%v", onCallGroupName, offset)
+		path := fmt.Sprintf("xxxxxxxxxxname=%s&limit=10&offset=%v", onCallGroupName, offset)
 		if templatesBody, err := f.GetInfos(path); err == nil {
 			var templates []TemplatesByOnCallGroStruct
 			err = json.Unmarshal(*templatesBody, &templates)
@@ -429,35 +326,15 @@ wait:
 
 type TemplateByIDStruct struct {
 	Template struct {
-		ID           int       `json:"id"`
-		Name         string    `json:"name"`
-		ParentID     int       `json:"parent_id"`
-		ActionID     int       `json:"action_id"`
-		CreateUserID int       `json:"create_user_id"`
-		CreateTime   time.Time `json:"create_time"`
-		Type         int       `json:"type"`
-		CommonLabels string    `json:"common_labels"`
-		OrgID        int       `json:"org_id"`
+		xxxxxxxx
 	} `json:"template"`
 	Action struct {
-		ID                 int    `json:"id"`
-		Uic                string `json:"uic"`
-		URL                string `json:"url"`
-		SendSms            int    `json:"send_sms"`
-		SendMail           int    `json:"send_mail"`
-		Callback           int    `json:"callback"`
-		BeforeCallbackMail int    `json:"before_callback_mail"`
-		AfterCallbackMail  int    `json:"after_callback_mail"`
-		OncallGroupID      int    `json:"oncall_group_id"`
-		OncallGroups       []struct {
-			ID   int    `json:"id"`
-			Name string `json:"name"`
-		} `json:"oncall_groups"`
+		xxxxxxxx
 	} `json:"action"`
 }
 
 func (f *FALCON) GetTemplatesByID (templateID int) (*TemplateByIDStruct, error) {
-	path := fmt.Sprintf("/template?id=%v", templateID)
+	path := fmt.Sprintf("/xxxxxx?id=%v", templateID)
 	if body, err := f.GetInfos(path); err == nil {
 		var templateByID TemplateByIDStruct
 		err = json.Unmarshal(*body, &templateByID)
@@ -487,8 +364,8 @@ func (f *FALCON) ChangeTemandStraTags (tags *string, modList *[]string) (string,
 		// 如果有标签 owt=mfe，则不进行修改
 		if key == "owt" {
 			for _, val := range vals {
-				if val == "mfe" {
-					//fmt.Println("tag 含有 mfe")
+				if val == "xxxx" {
+					//fmt.Println("tag 含有 xxx")
 					return "", true
 				}
 			}
@@ -507,7 +384,7 @@ func (f *FALCON) ChangeTemandStraTags (tags *string, modList *[]string) (string,
 					//return "", false
 				}
 
-				// 如果 匹配值 c4 存在，则添加目标值 ak
+				// 如果 匹配值 xxxx 存在，则添加目标值 xxxx
 				if val == (*modList)[1] {
 					val = val + "|" + (*modList)[2]
 					keyExitFlag = true
@@ -534,7 +411,7 @@ func (f *FALCON) ChangeTemandStraTags (tags *string, modList *[]string) (string,
 func (f *FALCON) ModTemplate (templateInfo *TemplateByIDStruct) bool {
 
 	// 修改之前存储一下日志
-	recordPath := fmt.Sprintf("/template?id=%v",  (*templateInfo).Template.ID)
+	recordPath := fmt.Sprintf("xxxxe?id=%v",  (*templateInfo).Template.ID)
 	if body, err := f.GetInfos(recordPath); err == nil {
 		if ok := f.Logging("template", body); !ok {
 			fmt.Println("记录模板日志失败")
@@ -556,27 +433,9 @@ func (f *FALCON) ModTemplate (templateInfo *TemplateByIDStruct) bool {
 }
 
 type StrategyByTempIDStruct struct {
-	ID         int    `json:"id"`
-	Note       string `json:"note"`
-	MaxStep    int    `json:"max_step"`
-	TplID      int    `json:"tpl_id"`
-	TplName    string `json:"tpl_name"`
-	Type       int    `json:"type"`
-	Period     int    `json:"period"`
+	xxxx
 	Strategies []struct {
-		ID              int    `json:"id"`
-		Metric          string `json:"metric"`
-		Tags            string `json:"tags"`
-		ExcludeTags     string `json:"exclude_tags"`
-		Func            string `json:"func"`
-		Op              string `json:"op"`
-		Note            string `json:"note"`
-		Condition       interface{}    `json:"condition"`
-		Priority        int    `json:"priority"`
-		Period          int    `json:"period"`
-		RunBegin        string `json:"run_begin"`
-		RunEnd          string `json:"run_end"`
-		StrategyGroupID int    `json:"strategy_group_id"`
+		xxxxx
 	} `json:"strategies"`
 	Pause int `json:"pause"`
 }
@@ -650,36 +509,10 @@ func (f *FALCON) GetStrategyByTempID (templateID int) (*[]StrategyByTempIDStruct
 
 func (f *FALCON) ModStrategy (strategies *StrategyByTempIDStruct) bool {
 	var putUpdateStrategies PutUpdateStrategies
-
-	putUpdateStrategies.ID = strategies.ID
-	putUpdateStrategies.Note = (*strategies).Note
-	putUpdateStrategies.MaxStep = (*strategies).MaxStep
-	putUpdateStrategies.TplName = (*strategies).TplName
-	putUpdateStrategies.TplID = (*strategies).TplID
-	putUpdateStrategies.Type = (*strategies).Type
-	putUpdateStrategies.Period = (*strategies).Period
-	putUpdateStrategies.Pause = (*strategies).Pause
-
-	for _, strategy := range (*strategies).Strategies {
-		var updateStrategy UpdateStrategy
-		updateStrategy.ID = strategy.ID
-		updateStrategy.Metric = strategy.Metric
-		updateStrategy.Tags = strategy.Tags
-		updateStrategy.ExcludeTags = strategy.ExcludeTags
-		updateStrategy.Func = strategy.Func
-		updateStrategy.Op = strategy.Op
-		updateStrategy.Note = strategy.Note
-		updateStrategy.Condition = strategy.Condition
-		updateStrategy.Priority = strategy.Priority
-		updateStrategy.Period = strategy.Period
-		updateStrategy.RunBegin = strategy.RunBegin
-		updateStrategy.RunEnd = strategy.RunEnd
-		updateStrategy.StrategyGroupID = strategy.StrategyGroupID
-		putUpdateStrategies.UpdateStrategies = append(putUpdateStrategies.UpdateStrategies, updateStrategy)
-	}
+xxxxxxxxxxxxxx
 
 	// 修改之前存储一下日志
-	recordPath := fmt.Sprintf("/strategy?id=%v", strategies.ID)
+	recordPath := fmt.Sprintf("/xxxxx?id=%v", strategies.ID)
 	if body, err := f.GetInfos(recordPath); err == nil {
 		if ok := f.Logging("strategy", body); !ok {
 			fmt.Println("记录策略日志失败：")
@@ -690,7 +523,7 @@ func (f *FALCON) ModStrategy (strategies *StrategyByTempIDStruct) bool {
 		return false
 	}
 
-	path := fmt.Sprintf("/strategy/%d", strategies.ID)
+	path := fmt.Sprintf("/xxxx/%d", strategies.ID)
 	if res, err := f.PutInfos(path, putUpdateStrategies); err == nil {
 		fmt.Println("更新成功的策略： ", string(*res))
 		return true
@@ -701,45 +534,17 @@ func (f *FALCON) ModStrategy (strategies *StrategyByTempIDStruct) bool {
 }
 
 type PutUpdateStrategies struct {
-	ID         int    `json:"id"`
-	Note          string `json:"note"`
-	MaxStep       int    `json:"max_step"`
-	TplID         int    `json:"tpl_id"`
-	TplName    string `json:"tpl_name"`
-	Type          int    `json:"type"`
-	Period        int    `json:"period"`
-	Pause int `json:"pause"`
+xxxxxx
 	AddStrategies []struct {
 		Metric      string `json:"metric"`
-		Tags        string `json:"tags"`
-		ExcludeTags string `json:"exclude_tags"`
-		Func        string `json:"func"`
-		Op          string `json:"op"`
-		Note            string `json:"note"`
-		Condition   interface{}    `json:"condition"`
-		Priority    int    `json:"priority"`
-		RunBegin    string `json:"run_begin"`
-		RunEnd      string `json:"run_end"`
-		StrategyGroupID int    `json:"strategy_group_id"`
+	xxxxx
 	} `json:"add_strategies"`
-	UpdateStrategies []UpdateStrategy `json:"update_strategies"`
-	DeleteStrategies []int `json:"delete_strategies"`
+	xxxxxxxxe_strategies"`
 }
 
 type UpdateStrategy struct {
 	ID          int    `json:"id"`
-	Metric      string `json:"metric"`
-	Tags        string `json:"tags"`
-	ExcludeTags string `json:"exclude_tags"`
-	Func        string `json:"func"`
-	Op          string `json:"op"`
-	Note        string `json:"note"`
-	Condition   interface{}    `json:"condition"`
-	Priority    int    `json:"priority"`
-	Period      int    `json:"period"`
-	RunBegin    string `json:"run_begin"`
-	RunEnd      string `json:"run_end"`
-	StrategyGroupID int    `json:"strategy_group_id"`
+xxxxxxxxxx
 }
 
 func (f *FALCON) ModStrategyByTempID (templateID int, modList *[]string) bool {
